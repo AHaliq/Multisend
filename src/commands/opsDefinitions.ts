e2e672b2d70e992c3b630b1e567185f7f7bf0d4b
Abdul Haliq <haliq@Abduls-MacBook-Pro.local>
2023-07-16 15:39:13 +0800

    feat(fundupto): added flag to fund up to amount

--- src/commands/opsDefinitions.ts
+++ src/commands/opsDefinitions.ts
@@ -59,7 +59,7 @@ const balance: op = {
 const fund: op = {
   name: 'fund',
   description: 'Fund all transaction wallets.',
-  help: `args parsed as: [amount](e[n]|g) --contract [contractAddress], 
+  help: `args parsed as: [amount](e[n]|g) --upto --contract [contractAddress], 
 if g; gwei, then n=9, if no g and e then n=18
 thus 'amount * 10^n' is the amount in wei. e.g.
 9 -> 9000000000000000000 wei
@@ -67,7 +67,10 @@ thus 'amount * 10^n' is the amount in wei. e.g.
 9e3 -> 9000 wei
 
 optional --contract [contractAddress] to fund a specific token contract
-if no contract specified will fund native token`,
+if no contract specified will fund native token
+
+if upto flag present rather than funding amount,
+it will fund to match balance to minimum of amount value`,
   parseArgs: (arg: string) => {
     const args = arg.split(' ');
     if (args[0] === undefined) {
@@ -86,23 +89,42 @@ if no contract specified will fund native token`,
       }
       return { val, contractAddress, mutex: new Mutex() };
     }
-    return { val, contractAddress: '0x', mutex: new Mutex() };
+
+    const upto = args.indexOf('--upto') !== -1;
+    return { val, contractAddress: '0x', mutex: new Mutex(), upto };
   },
-  func: async ({ funding, transacting, args, out }) => {
-    const { val, contractAddress, mutex } = args as {
+  func: async ({ funding, transacting, gasPrice, args, out }) => {
+    const { val, contractAddress, mutex, upto } = args as {
       val: bigint;
       contractAddress: string;
       mutex: Mutex;
+      upto: boolean;
     };
     out('waiting');
     const release = await mutex.acquire();
     out('running');
     // enter protected section
+    const { provider } = transacting;
+    if (provider === null || provider === undefined) {
+      throw new Error('Drain: Network failed to create provider');
+    }
 
     if (contractAddress === '0x') {
+      let amountToSend;
+      if (upto) {
+        const balance = await provider.getBalance(transacting.address);
+        if (balance > val) {
+          return 'already satisfied';
+        }
+        amountToSend = val - balance;
+      } else {
+        amountToSend = val;
+      }
+
       const res = await funding.sendTransaction({
         to: transacting.address,
-        value: val,
+        value: amountToSend,
+        gasPrice,
       });
       const rec = await res.wait();
       release();
@@ -110,7 +132,19 @@ if no contract specified will fund native token`,
       return rec.hash;
     }
     const contract = ERC20__factory.connect(contractAddress, funding.provider);
-    const res = await contract.transfer(transacting.address, val);
+    let amountToSend;
+    if (upto) {
+      const balance = await contract.balanceOf(transacting.address);
+      if (balance > val) {
+        return 'already satisfied';
+      }
+      amountToSend = val - balance;
+    } else {
+      amountToSend = val;
+    }
+    const res = await contract.transfer(transacting.address, amountToSend, {
+      gasPrice,
+    });
     const rec = await res.wait();
     release();
     if (rec === null) throw new Error('failed to fund');
@@ -121,9 +155,10 @@ if no contract specified will fund native token`,
 const drain: op = {
   name: 'drain',
   description: 'Drain all transaction wallets to a target wallet.',
-  help: `args parsed as: [targetWalletAddress] --amount [amount] --contract [contractAddress]
+  help: `args parsed as: [targetWalletAddress] --amount [amount] --upto [upto] --contract [contractAddress]
 if no amount specified will drain all
-if no contract specified will drain native token`,
+if no contract specified will drain native token
+upto tries to drain all but leave behind upto amount;transfers: balance - upto`,
   parseArgs: (arg: string) => {
     const args = arg.split(' ');
     const target = args[0];
@@ -141,24 +176,28 @@ if no contract specified will drain native token`,
     // validate contract
 
     const amountIndex = args.indexOf('--amount');
-    const amount = amountIndex === -1 ? 0 : Number(args[amountIndex + 1]);
-    if (amount < 0) {
-      throw new Error('Drain: amount must be a positive number');
-    }
+    const amount =
+      amountIndex === -1 ? 0n : parseAmt(args[amountIndex + 1] ?? '0');
+    // validate amount
+
+    const uptoIndex = args.indexOf('--upto');
+    const upto = uptoIndex === -1 ? 0n : parseAmt(args[uptoIndex + 1] ?? '0');
     // validate amount
 
     return {
       isNative,
       contractAddress,
       amount,
+      upto,
       target,
     };
   },
   func: async ({ transacting, gasPrice, args }) => {
-    const { isNative, contractAddress, amount, target } = args as {
+    const { isNative, contractAddress, amount, target, upto } = args as {
       isNative: boolean;
       contractAddress: string;
-      amount: number;
+      amount: bigint;
+      upto: bigint;
       target: string;
     };
     const { provider } = transacting;
@@ -168,42 +207,31 @@ if no contract specified will drain native token`,
 
     if (isNative) {
       let amountToSend;
-      if (amount === 0) {
+      if (upto !== 0n || amount === 0n) {
         const balance = await provider.getBalance(transacting.address);
-        // Get balance
-
-        const gasLimitEstimate = await transacting.estimateGas({
-          to: target,
-          value: balance,
-        });
-        const gasCostEstimate = gasPrice * gasLimitEstimate;
-        // Estimate gas cost
-
-        amountToSend = balance - gasCostEstimate;
-        // Calculate transfer amount
+        amountToSend = balance - upto;
       } else {
-        amountToSend = ethers.parseEther(String(amount));
+        amountToSend = amount;
       }
-
       return (
         await transacting.sendTransaction({
           to: target,
           value: amountToSend,
+          gasPrice,
         })
       ).hash;
-      // Send transaction
     }
     // drain native
 
     const contract = ERC20__factory.connect(contractAddress, provider);
-    return (
-      await contract.transfer(
-        target,
-        amount === 0
-          ? await contract.balanceOf(transacting.address)
-          : ethers.parseEther(String(amount)),
-      )
-    ).hash;
+    let amountToSend;
+    if (upto !== 0n || amount === 0n) {
+      const balance = await contract.balanceOf(transacting.address);
+      amountToSend = balance - upto;
+    } else {
+      amountToSend = amount;
+    }
+    return (await contract.transfer(target, amountToSend, { gasPrice })).hash;
     // drain erc20
   },
 };
